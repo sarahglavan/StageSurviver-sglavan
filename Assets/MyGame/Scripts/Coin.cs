29378e1c9d3582a559dc25896e6a57caf3abf1ee
Sarah Glavan <s.glavan@sarahglavan-mac.local>
2019-01-09 15:13:48 +0100

    V2 09.01.19

---
+++ Assets/MyGame/Scripts/Coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+// Soll coins die eingesammelt werden zählen
---
+++ Assets/MyGame/Scripts/Tomatoe.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tomatoe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+// wenn dich Tomaten treffen soll man ein Leben Leben verlieren. Der Counter 
+// soll von drei Leben runterzählen
